75cb2994efff49fe54601ff0a242495b64b3784c
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-11 15:06:41 +0200

    (INHERITANCE)Created a parent class

---
+++ src/Inheritance.java
@@ -0,0 +1,16 @@
+public class Inheritance {
+    class Character {
+        String name;
+        int health;
+
+        void move() {
+            System.out.println(name + "moves around");
+        }
+
+        void attacks() {
+            System.out.println(name + "attacks");
+        }
+    }
+
+
+}
