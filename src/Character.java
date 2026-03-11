2ae8332928e7c4eb6607a2e98035093faa91fa22
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-11 15:50:13 +0200

    Updated main class and created parent class

--- src/Character.java
+++ src/Character.java
@@ -9,12 +9,5 @@ public class Character {
         void attacks() {
             System.out.println(name + "attacks");
         }
-    }}
-//
-////SINGLE INHERITANCE?
-//    class Warrior extends Character {
-//        void useSword(){
-//            System.out.println(name );
-//        }
-//}
-//}
+    }
+
