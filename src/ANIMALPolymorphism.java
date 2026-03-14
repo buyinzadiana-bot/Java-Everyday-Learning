58eb7fa5e659f57b8c7adbc91aae9dec5039b0bc
Diana BUYINZA <buyinzadiana@gmail.com>
2026-03-14 11:31:12 +0200

    Created Animal class parent class(polymorphism)

--- src/ANIMALPolymorphism.java
+++ src/ANIMALPolymorphism.java
@@ -1,2 +1,14 @@
 public class ANIMALPolymorphism {
+    private String name;
+    private String color;
+
+    public void eat(){
+        System.out.println("Munch");
+    }
+    public String getName(){
+        return name;
+    }
+    public String setName(String name){
+        this.name = name;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-//        Inheritance = when one class inherits the attribute and methods from another class
-//        child parent relationship
-        Warrior hero =new Warrior ();
-        hero.name = "Diana";
-        hero.attacks();
-        hero.move();
+////        Inheritance = when one class inherits the attribute and methods from another class
+////        child parent relationship
+//        Warrior hero =new Warrior ();
+//        hero.name = "Diana";
+//        hero.attacks();
+//        hero.move();
+
+        Animal myAnimal = new Animal();
     }
 }
